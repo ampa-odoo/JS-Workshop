89c4cd61da4b047acc84485257c361a83f6f1dcd
Aman Patel <ampa@odoo.com>
2023-03-16 19:00:12 +0530

    Day 4

--- workshop-day3/music_player/controllers/controllers.py
+++ workshop-day3/music_player/controllers/controllers.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 from odoo import http
+from odoo.modules.module import get_module_resource
 import json
 
 class MusicPlayer(http.Controller):
@@ -7,25 +8,16 @@ class MusicPlayer(http.Controller):
     def index(self, **kw):
         return http.request.render('music_player.music_template')
 
-    @http.route('/music/search', auth='public',type="http", methods=["GET"])
+    @http.route('/music/search', auth='public',type="http",methods=['GET'])
     def search(self, **kw):
-        song_name = kw.get('song_name')
-        print("---------------------",song_name)
-        musics = http.request.env['music_player.music_player'].search_read([('name', 'ilike', song_name)],fields={"name", "url"})
-        if not musics:
-            musics = "Song not Found"
-        # breakpoint()
-        return Response(json.dumps({'result': musics}), content_type='application/json')
+            song_name = kw.get('song_name')
+            songs = http.request.env['music_player.music_player'].search_read([('name','ilike',song_name)],fields=["name","url"])
+            if not songs:
+                songs = "Song not found."
+            return http.Response(json.dumps({'result':songs}),content_type='application/json')
 
-#     @http.route('/music_player/music_player/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('music_player.listing', {
-#             'root': '/music_player/music_player',
-#             'objects': http.request.env['music_player.music_player'].search([]),
-#         })
-
-#     @http.route('/music_player/music_player/objects/<model("music_player.music_player"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('music_player.object', {
-#             'object': obj
-#         })
+    @http.route('/music/<model("music_player.music_player"):music>',type="http",auth="public",methods=['GET'])
+    def load(self,music,**kw):
+        music_file_path = get_module_resource('music_player','static/songs',music.filename)
+        file = open(music_file_path,'rb').read()
+        return file
--- workshop-day3/music_player/static/app.js
+++ workshop-day3/music_player/static/app.js
@@ -1,48 +1,148 @@
 /** @odoo-module**/
 
-const { Component,xml,mount } = owl;
+const { Component,xml,mount,useState,onWillStart } = owl;
 
-// class List extends Component {
-//     static template = xml`
-//         <div>
-
-//         </div>
-//     `;
-//     static props = ['searchData'];
-// }
-
-class Search extends Component {
+let audio='';
+class Player extends Component {
     static template = xml`
-        <div style="border:1px solid black; text-align:center">
-            <input type="text" id="searchSong" placeholder="Search Music" value="Freedom"/>
-            <button t-on-click="getMusic" id="SearchButton">Search</button>
+        <div style="position:absolute;bottom:0px">
+            <h2 id="song-title">Song Title</h2>
+            <div>
+                <button id="pause-button" t-on-click="pauseThisSong">Pause</button>
+                <button id="play-button" t-on-click="playThisSong">Play</button>
+                <button id="stop-button" t-on-click="stopThisSong">Stop</button>
+            </div>
+        </div>
+    `;
+    playThisSong(){
+        if(!audio){
+            return;
+        }
+        audio.play();
+    }
+    pauseThisSong(){
+        if(!audio){
+            return;
+        }
+        audio.pause();
+    }
+    stopThisSong(){
+        if(!audio){
+            return;
+        }
+        audio.pause();
+        audio.currentTime=0;
+    }
+}
+
+class PlayList extends Component {
+    static template = xml`
+    <div id="PlayList" style="float:right">
+        <h2>My Playlist</h2>
+        <t t-if="props.playData[0]">     
+            <t t-foreach="props.playData" t-as="song" t-key="song.id">
+                <p><t t-out="song.name"/></p>
+                <button t-att-value="song.url" t-on-click="removeSongFromPlayList">Remove from playlist</button>
+                <button t-att-value="song.url" t-on-click="playSong">Play Song</button>
+            </t>
+        </t>
+    </div>
+    `;
+
+    removeSongFromPlayList(ev) {
+        const selectedSongUrl = ev.target.getAttribute('value');
+        const selectedSong = this.props.playData.findIndex(song => song.url===selectedSongUrl);
+        this.props.playData.splice(selectedSong,1);
+    }
+
+    playSong(ev) {
+        if (audio) {
+            audio.pause();
+            audio.currentTime=0;
+        }
+        const selectedSongUrl = ev.target.getAttribute('value');
+        const selectedSong = this.props.playData[0].find(song => song.url===selectedSongUrl);
+        document.getElementById("song-title").textContent = selectedSong.name;
+        audio = new Audio(selectedSongUrl);
+        audio.play();
+    }
+}
+
+class List extends Component {
+    static template = xml`
+        <div id="MusicList" style="float:left">
+            <t t-if="props.searchData[0] and props.searchData[0] !== 'Song not found.'">
+                <h2>List of Songs</h2>
+                <t t-foreach="props.searchData[0]" t-as="song" t-key="song.id">
+                    <p><t t-out="song.name"/></p>
+                    <button t-att-value="song.url" t-on-click="addSongToPlaylist">Add to playlist</button>
+                    <button t-att-value="song.url" t-on-click="playSong">Play Song</button>
+                </t>
+            </t>
+            <Player/>
         </div>
     `;
 
-    // setup() {
-    //     this.searchData = useState([]);
-    // }
+    playSong(ev) {
+        if (audio) {
+            audio.pause();
+            audio.currentTime=0;
+        }
+        const selectedSongUrl = ev.target.getAttribute('value');
+        const selectedSong = this.props.searchData[0].find(song => song.url===selectedSongUrl);
+        document.getElementById("song-title").textContent = selectedSong.name;
+        audio = new Audio(selectedSongUrl);
+        audio.play();
+    }
+
+    addSongToPlaylist(ev) {
+
+        const selectedSongUrl = ev.target.getAttribute('value');
+        const selectedSong = this.props.searchData[0].find(song => song.url===selectedSongUrl);
+        this.props.addToPlayList(selectedSong);
+    }
+
+    static components = {Player};
+}
+
+class Search extends Component {
+    static template = xml`
+        <div style="border:1px,solid,black; text-align:center">
+            <input type="text" id="searchSong" placeholder="Search Music" value="Freedom"/>
+            <button t-on-click="getMusic" id="SearchButton">Search</button>
+            <List searchData="searchData" addToPlayList="props.addToPlayList" playData="props.playData"/>
+        </div>
+    `;
+
+    setup() {
+        this.searchData = useState([]);
+    }
 
     async getMusic() {
         const findSong = document.getElementById("searchSong").value;
         const response = await fetch(`/music/search?song_name=${findSong}`);
-        console.log(response.body);
         const {result: newData} = await response.json();
-        console.log(newData);
-        // this.searchData.push(newData);
+        this.searchData.push(newData);
     }
-
-    // static components = {List};
+    static components = {List};
 }
 
 class Root extends Component {
     static template = xml`
     <div>
-        <Search/>
+        <Search addToPlayList="this.addToPlayList" playData="playData"/>
+        <PlayList playData="playData"/>
     </div>
     `;
 
-    static components = {Search};
+    setup() {
+            this.playData = useState([]);
+    }
+
+    addToPlayList(song) {
+        this.playData.push(song);
+    }
+    static components = {Search,PlayList};
 }
 
 window.onload = function() {
